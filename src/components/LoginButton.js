af6b132a06a6f6d428427a7709d15ac747d6fd9f
Kevin Kim <kevink856@gmail.com>
2023-05-22 12:42:05 -0700

    Store keys in .env
    Added .env support and .env.example for reference for sensitive information

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,9 +1,11 @@
 // Import dependencies
 import "../styles/LogButton.css"
 
+// ENV
+require ("dotenv").config();
+
 // Declare consts for Oauth link formatting
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const CLIENT_ID = "3f8575d48b8048139be97f96b54f41ad";
 const REDIRECT_URI = "https://kevink856.github.io/communifyy/home";
 const RESPONSE_TYPE = "token"
 
@@ -15,7 +17,7 @@ const LoginButton = () => {
         <button className = "Log-button"
             type = "button"
             onClick = {() => {
-                window.location.href = `${AUTH_ENDPOINT}?response_type=${RESPONSE_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+                window.location.href = `${AUTH_ENDPOINT}?response_type=${RESPONSE_TYPE}&client_id=${process.env.CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
             }}
         > Login to Spotify
         </button>
