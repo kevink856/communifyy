19ea0bc2b8312e3088aa6409c98269ff6ef0b120
Kevin Kim <kevink856@gmail.com>
2023-05-22 13:09:26 -0700

    Fix .env access
    Removed .env require statement
     - Redundant in React App

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,9 +1,6 @@
 // Import dependencies
 import "../styles/LogButton.css"
 
-// ENV
-require ("dotenv").config();
-
 // Declare consts for Oauth link formatting
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const REDIRECT_URI = "https://kevink856.github.io/communifyy/home";
@@ -17,7 +14,7 @@ const LoginButton = () => {
         <button className = "Log-button"
             type = "button"
             onClick = {() => {
-                window.location.href = `${AUTH_ENDPOINT}?response_type=${RESPONSE_TYPE}&client_id=${process.env.CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+                window.location.href = `${AUTH_ENDPOINT}?response_type=${RESPONSE_TYPE}&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
             }}
         > Login to Spotify
         </button>
