ff538d2d2c2de543f517b2a892af3220879aaa80
Kevin Kim <kevink856@gmail.com>
2023-06-06 09:29:10 -0700

    Fix Infinite Loop
    Call fetch only in useEffect[] to run only on initialization

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,19 +16,23 @@ const Home = () => {
     const [topsongs, setTopsongs] = useState("");
     const [topartists, setTopartists] = useState("");
 
-    // Run and parse token from URL whenever "token" changes
-    useEffect(() => {
-        const hash = window.location.hash;
-        let token = window.localStorage.getItem("token");
+    // Post user's top 5 songs
+    const postTop = async () => {
+        await fetch(`${process.env.REACT_APP_BASE_URL}/top`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                id, topsongs: topsongs.split(","), topartists: topartists.split(",")
+            })
+        }).then(resp => resp.json());
+        setId("");
+        setTopsongs("");
+        setTopartists("");
+    }
 
-        if(!token && hash) {
-            token = hash.substring(1).split("&").find(e => e.startsWith("access_token")).split("=")[1];
-            window.location.hash = "";
-            window.localStorage.setItem("token", token);
-        }
-        setToken(token);
-    }, [token]);
-
+    // Fetch specific user info
     const fetchData = async () => {
         // Gets user's profile data
         const result = await fetch("https://api.spotify.com/v1/me", {
@@ -46,26 +50,21 @@ const Home = () => {
         const top = await result.json();
     }
 
-    const postTop = async () => {
-        await fetch(`${process.env.REACT_APP_BASE_URL}/top`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                id, topsongs: topsongs.split(","), topartists: topartists.split(",")
-            })
-        }).then(resp => resp.json());
-        setId("");
-        setTopsongs("");
-        setTopartists("");
-    }
+    // Run and parse token from URL whenever "token" changes
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
 
-    // Fetch necessary user info
-    fetchData();
+        if(!token && hash) {
+            token = hash.substring(1).split("&").find(e => e.startsWith("access_token")).split("=")[1];
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+        }
+        setToken(token);
 
-    // Post user's top 5 songs
-    postTop();
+        postTop();
+        fetchData();
+    }, []);
 
     return (
         <div className = "Home">
